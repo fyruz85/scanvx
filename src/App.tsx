58a8f7372f6895f4f2e3725cf48dd5333f412b9e
fyruz85 <117002247+fyruz85@users.noreply.github.com>
2026-07-15 09:50:12 +0800

    integrate: update src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Header } from './components/Header';
 import { Hero } from './components/Hero';
 import { Services } from './components/Services';
+import { WhyUs } from './components/WhyUs';
 import { Approach } from './components/Approach';
 import { Projects } from './components/Projects';
-import { Blog } from './components/Blog';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 import { LiveChat } from './components/LiveChat';
@@ -16,9 +16,9 @@ function App() {
       <main>
         <Hero />
         <Services />
+        <WhyUs />
         <Approach />
         <Projects />
-        <Blog />
         <Contact />
       </main>
       <Footer />
